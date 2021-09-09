cc0ca58d46c957b0cc1824233b239fb328c324f7
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2021-09-08 21:53:03 -0300

    route create account

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+   const {name, username} = request.body;
+
+    const userAlreadyExists = users.some(
+      (user)=> user.username === username
+  );
+
+  if(userAlreadyExists){
+      return res.status(400).json({error: "Usuario já cadastrado!"})
+  }else{
+        objUser = {
+          id: uuidv4(),
+          name,
+          username,
+          todos: []
+        }
+        users.push(objUser);
+
+    response.status(201).json(objUser);
+
+      
+  }
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
